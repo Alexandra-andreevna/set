49f6bba98c5fe37cae1b47e4f018672512da0b76
Alexandra-andreevna <Alexandra25andreevna@gmail.com>
2015-04-25 18:58:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ int main (void)
 {
 	Set < int > set1;
 
-	int y = 9;
-
-	set1.add(y);
-	set1.remove(y);
-	cout<<set1.contains(y);
-
+	set1.add(9);
+	set1.add(5);
+	set1.add(3);
+	set1.remove(3);
+	cout<<set1.contains(5);
+	cout<<set1.size();
 }
